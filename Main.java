7526d7ac4023d45bd9237a19fcd275cbf12609a4
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-08 14:36:08 +0530

    adding Binary search tree

--- Main.java
+++ Main.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
-        tree.prettyDisplay();
-        tree.preOrder();
-        tree.inOrder();
-        tree.postOrder();
-
-//        BST tree = new BST();
-//        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
-//        int[] nums2 = { 10, 20, 15, 13, 12};
-//        tree.populate(nums2);
-//        tree.display();
+//        Scanner scanner = new Scanner(System.in);
+//        BinaryTree tree = new BinaryTree();
+//        tree.populate(scanner);
+//        tree.prettyDisplay();
+//        tree.preOrder();
 //        tree.inOrder();
+//        tree.postOrder();
+
+        BST tree = new BST();
+        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+        int[] nums2 = { 10, 20, 15, 13, 12};
+        tree.populate(nums2);
+        tree.display();
     }
 }
