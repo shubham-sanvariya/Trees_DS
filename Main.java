86b3f7e4df6c9ce75521b3fce2fc3ea424dce505
shubham-sanvariya <shubhamsanvariyait.com>
2023-11-16 22:39:08 +0530

    adding implementation of AVL trees

--- Main.java
+++ Main.java
@@ -10,10 +10,19 @@ public class Main {
 //        tree.inOrder();
 //        tree.postOrder();
 
-        BST tree = new BST();
-        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
-        int[] nums2 = { 10, 20, 15, 13, 12};
-        tree.populate(nums2);
+//        BST tree = new BST();
+//        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+//        int[] nums2 = { 10, 20, 15, 13, 12};
+//        tree.populate(nums2);
+//        tree.display();
+
+        Tree2AVL tree = new Tree2AVL();
+
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+
+        System.out.println(tree.height());
         tree.display();
     }
 }
